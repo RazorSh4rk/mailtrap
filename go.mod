75e299be6693766591ebb0a806087a8325d14f98
RazorSh4rk <iamrazorshark@gmail.com>
2026-01-17 02:01:33 -0800

    funky

--- go.mod
+++ go.mod
@@ -2,9 +2,12 @@ module github.com/RazorSh4rk/mailtrap
 
 go 1.25.5
 
+require github.com/phires/go-guerrilla v1.6.7
+
 require (
 	github.com/asaskevich/EventBus v0.0.0-20200907212545-49d423059eef // indirect
-	github.com/flashmob/go-guerrilla v1.6.1 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,19 +2,51 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/flashmob/go-guerrilla"
+	"github.com/phires/go-guerrilla"
+	"github.com/phires/go-guerrilla/backends"
+	"github.com/phires/go-guerrilla/mail"
 )
 
+var LoggerProcessor = func() backends.Decorator {
+	initFunc := backends.InitializeWith(func(backendConfig backends.BackendConfig) error {
+		backends.Log().Info("Logger processor loaded")
+		return nil
+	})
+	backends.Svc.AddInitializer(initFunc)
+	return func(p backends.Processor) backends.Processor {
+		return backends.ProcessWith(
+			func(e *mail.Envelope, task backends.SelectTask) (backends.Result, error) {
+				backends.Log().Info(e.String())
+				return p.Process(e, task)
+			},
+		)
+	}
+}
+
 func main() {
-	d := guerrilla.Daemon{}
+	cfg := &guerrilla.AppConfig{
+		Servers: []guerrilla.ServerConfig{
+			{
+				ListenInterface: "0.0.0.0:25",
+				IsEnabled:       true,
+			},
+		},
+		AllowedHosts: []string{
+			"*",
+		},
+		BackendConfig: backends.BackendConfig{
+			"save_process":     "HeadersParser|Debugger|FullLogger",
+			"validate_process": "FullLogger",
+		},
+	}
+
+	d := guerrilla.Daemon{Config: cfg}
+	d.AddProcessor("FullLogger", LoggerProcessor)
 	err := d.Start()
 
 	if err == nil {
 		fmt.Println("Server Started!")
-		hName, _ := os.Hostname()
-		fmt.Println("Host is", hName)
 	}
 
 	for {
